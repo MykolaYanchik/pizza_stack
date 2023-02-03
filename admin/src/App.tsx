28055c42eecc1af829d060f8238e69e466762238
Mykola Yanchyk <mikola.yanchik@gmail.com>
2023-02-03 11:51:42 +0200

    admin: install react and basic styles

---
+++ admin/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./assets/styles/style.scss"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Pizza
+      </header>
+    </div>
+  );
+}
+
+export default App;
